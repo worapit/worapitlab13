f0b2d413e107a6658167347267adba391bf1babf
worapit <drm.wmwork@gmail.com>
2021-12-23 22:41:13 +0700

    complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
 using namespace std;
 
+
+int fibonacci(int N);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int N){
+    if(N == 0 || N == 1){
+        return N;
+    }else if(N > 1){
+         return fibonacci(N-1)+fibonacci(N-2) ;
+    }
+    return N;
+       
 }
