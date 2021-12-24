341b1963d909458cdfe664d8fd9917fa4d41b8dc
worapit <drm.wmwork@gmail.com>
2021-12-24 16:10:24 +0700

    complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,17 +1,21 @@
 #include<iostream>
+#include<ctime>
 using namespace std;
 
 
-int fibonacci(int N);
+long long int fibonacci(int N);
+
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << (fibonacci(50)) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
 
-int fibonacci(int N){
+long long int fibonacci(int N){
     if(N == 0 || N == 1){
         return N;
     }else if(N > 1){
